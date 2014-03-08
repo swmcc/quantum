4369fd70a77c7ab812a238eb17a8c41253070717
Stephen McCullough <me@swm.cc>
2014-03-08 21:37:58 +0000

    Moved the loading of the files into a helper function

--- app.py
+++ app.py
@@ -3,12 +3,22 @@
 import json, glob 
 from pprint import pprint
 
-for file in glob.glob('data/js/tweets/*.js'):
-    with open(file) as data:
-        # skip that annoying first line
-        raw_data = data.readlines()[1:]
-        raw_data = "".join(raw_data)
-        raw_json = json.loads(raw_data)
-        data.close()
-        pprint(raw_json)
+def load_archive():
+    tweets = []
+    for file in glob.glob('data/js/tweets/*.js'):
+        with open(file) as data:
+            # skip that annoying first line
+            raw_data = data.readlines()[1:]
+            raw_data = "".join(raw_data)
+            raw_json = json.loads(raw_data)
+            data.close()
+            for tweet in raw_json:
+                tweets.append(tweet)
+    return sorted(tweets, key=lambda k: k['id'])
 
+def go_fish():
+    tweets = load_archive()
+    pprint(tweets)
+
+if __name__ == "__main__":
+    go_fish()
