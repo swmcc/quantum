b3c4ecb6d74e34f163aa985340f170cdbb47a6e4
Stephen McCullough <me@swm.cc>
2014-03-08 22:42:39 +0000

    Added in most_hashtags. Now outputs to a json file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import collections, json, glob
 from collections import Counter
 from pprint import pprint
 
+# Load the archive into memory
 def load_archive():
     tweets = []
     for file in glob.glob('data/js/tweets/*.js'):
@@ -17,18 +18,35 @@ def load_archive():
                 tweets.append(tweet)
     return sorted(tweets, key=lambda k: k['id'])
 
+# Most mentioned users I tweet about 
 def get_most_mentioned(tweets):
-    users = []
+    users = {} 
     for tweet in tweets:
         for user in tweet['entities']['user_mentions']:
             users.append(user['screen_name'])
     return collections.Counter(users).most_common(10)
 
+# Most hashtags I use
+def get_most_hashtags(tweets):
+    hashtags = []
+    for tweet in tweets:
+        for hashtag in tweet['entities']['hashtags']:
+            hashtags.append(hashtag['text'])
+    return collections.Counter(hashtags).most_common(10)
+
+# Output the results to a json file for consumption later
+def output_json(data):
+    with open('output.json', 'w') as file:
+        file.write(json.dumps(data, indent=4))
 
 def go_fish():
     tweets = load_archive()
-    most_mentioned_users = get_most_mentioned(tweets)
+    json = {
+      'most_mentioned_users': get_most_mentioned(tweets), 
+      'most_mentioned_hashtags': get_most_hashtags(tweets) 
+    }
 
+    output_json(json)
 
 if __name__ == "__main__":
     go_fish()
